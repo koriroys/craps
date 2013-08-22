dda9ad05c70156912911fa2bfb5af7c099f91267
Kori Roys <kori@koriroys.com>
2013-08-21 20:20:22 -0500

    oops, wrong spec description

--- spec/craps_spec.rb
+++ spec/craps_spec.rb
@@ -14,7 +14,7 @@ describe Craps do
     expect(output).to have_received(:puts).with("'natural' roll, you WIN!")
   end
 
-  it "wins if rolls a 3 on the first roll" do
+  it "loses if rolls a 3 on the first roll" do
     output = double('output').as_null_object
     die1 = Die.new
     die1.stub(:roll).and_return(1)
