f0ea5a5ff726cca95445d7935c3e472e4624c873
Kori Roys <kori@koriroys.com>
2013-08-21 19:00:55 -0500

    initial craps commit

---
+++ craps.rb
@@ -0,0 +1,59 @@
+class Die
+  def roll
+    rand(1..6)
+  end
+end
+ 
+die1 = Die.new
+die2 = Die.new
+# puts dice_roll = die1.roll + die2.roll
+first_roll = true
+ 
+class Player
+ 
+  def initialize
+    @point = nil
+  end
+ 
+  def pass dice_roll
+    (dice_roll == 7 || dice_roll == 11)
+  end
+ 
+  def no_pass dice_roll
+    dice_roll == 2 || dice_roll == 3 || dice_roll == 12
+  end
+ 
+  def roll (die1, die2, first_roll)
+ 
+    dice_roll = die1.roll + die2.roll
+    if first_roll
+      if pass (dice_roll)
+        puts dice_roll
+        puts "'natural' roll, you WIN!"
+      elsif no_pass(dice_roll)
+        puts dice_roll
+        puts "'craps!' you LOSE!"
+      else
+        @point = dice_roll
+        puts "set point is #{@point}"
+        die1 = Die.new
+        die2 = Die.new
+        roll(die1, die2, false)
+      end
+    else
+      puts "current roll is #{dice_roll}"
+      if dice_roll == 7
+        puts "you crapped out with a #{dice_roll}"
+      elsif dice_roll == @point
+        puts "you win with #{@point}"
+      else
+        die1 = Die.new
+        die2 = Die.new
+        roll(die1, die2, false)
+      end
+    end
+  end
+end
+ 
+ 
+Player.new.roll(die1, die2, true)
