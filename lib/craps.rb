8dcbea841fa03f203102c0374ef5d1546689b9f5
Kori Roys <kori@koriroys.com>
2013-08-21 19:59:24 -0500

    clean up whitespace

--- lib/craps.rb
+++ lib/craps.rb
@@ -5,20 +5,20 @@ class Die
 end
 
 class Player
- 
+
   def initialize(output)
     @point = nil
     @output = output
   end
- 
+
   def pass dice_roll
     (dice_roll == 7 || dice_roll == 11)
   end
- 
+
   def no_pass dice_roll
     dice_roll == 2 || dice_roll == 3 || dice_roll == 12
   end
- 
+
   def roll (die1, die2, first_roll)
     dice_roll = die1.roll + die2.roll
     if first_roll
